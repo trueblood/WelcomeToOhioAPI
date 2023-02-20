fbb2919eb55262d01c88a80a92203d866fc21415
Tim <timtrueblood@outlook.com>
2023-02-20 17:37:22 -0500

    updates

--- app/main.py
+++ app/main.py
@@ -88,7 +88,7 @@ def getXML():
    <![CDATA[<?xml version="1.0" encoding="UTF-8"?>
     <UiScreen>
       <Operations>
-      <Op type="Submit" action="start" ></Op>
+      <Op type="Submit" action="zeroxml" ></Op>
       <Op type="Back" action="./0.xml" ></Op>
     </Operations>
      <IoScreen>
