6ac3b1e4cd39a8613886a511e8e069f0da50c3ba
Tim <timtrueblood@outlook.com>
2023-02-24 15:14:01 -0500

    updates

--- app/main.py
+++ app/main.py
@@ -140,7 +140,7 @@ def getStepFour():
             <Ftp>
                <FtpParams>
                   <FileName>testfilestwo</FileName>
-                  <Host>50.63.92.108</Host>
+                  <Host>ftp.mytechieapps.com</Host>
                   <User>squeebit</User>
                   <Password>n*97Px1q</Password>
                   <StoreDir>scanned</StoreDir>
